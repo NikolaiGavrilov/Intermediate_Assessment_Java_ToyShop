292714e618823f5d5808b2ab9f1788545dd57e5b
Nikolay Gavrilov <nikolaisibir@mail.ru>
2024-01-26 13:48:45 +0300

    Created Toy class

---
+++ Toy.java
@@ -0,0 +1,40 @@
+public class Toy {
+    private int toyID;
+    private String toyName;
+    private double toyLootChance;
+
+    public Toy(int toyID, String toyName, double toyLootChance) {
+        this.toyID = toyID;
+        this.toyName = toyName;
+        this.toyLootChance = toyLootChance;
+    }
+
+    public int getToyID() {
+        return toyID;
+    }
+
+    public void setToyID(int toyID) {
+        this.toyID = toyID;
+    }
+
+    public String getToyName() {
+        return toyName;
+    }
+
+    public void setToyName(String toyName) {
+        this.toyName = toyName;
+    }
+
+    public double getToyLootChance() {
+        return toyLootChance;
+    }
+
+    public void setToyLootChance(double toyLootChance) {
+        this.toyLootChance = toyLootChance;
+    }
+
+    @Override
+    public String toString() {
+        return "Игрушка " + this.toyName + " (id: " + this.toyID + ", шанс выпадения: " + this.toyLootChance + ")\n";
+    }
+}
